24455ead963249d9f1bfa1cf5ee1b58272c8fe45
Juho Teperi <juho.teperi@iki.fi>
2017-01-12 19:46:29 +0200

    Run tests on Clojure 1.9

--- project.clj
+++ project.clj
@@ -20,5 +20,6 @@
           :defaults {:doc/format :markdown}}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
-  :aliases {"all" ["with-profile" "dev:dev,1.6:dev,1.7"]})
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha14"]]}}
+  :aliases {"all" ["with-profile" "dev:dev,1.6:dev,1.7:dev,1.9"]})

64d359c4d7c1e39a402a927c06a795fc0d3f4b77
Juho Teperi <juho.teperi@iki.fi>
2018-12-09 10:56:58 +0200

    Update deps

--- project.clj
+++ project.clj
@@ -7,19 +7,20 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
-  :dependencies [[org.clojure/core.memoize "0.5.9"]
-                 [ring/ring-core "1.5.1"]
-                 [cheshire "5.7.0"]
-                 [org.clojure/tools.reader "0.10.0"]
-                 [com.ibm.icu/icu4j "58.2"]
+  :dependencies [[org.clojure/core.memoize "0.7.1"]
+                 [ring/ring-core "1.7.1"]
+                 [cheshire "5.8.1"]
+                 [org.clojure/tools.reader "1.3.2"]
+                 [com.ibm.icu/icu4j "63.1"]
                  [circleci/clj-yaml "0.5.6"]
-                 [clojure-msgpack "1.2.0"]
-                 [com.cognitect/transit-clj "0.8.297"]]
+                 [clojure-msgpack "1.2.1"]
+                 [com.cognitect/transit-clj "0.8.313"]]
   :plugins [[lein-codox "0.10.2"]]
   :codox {:src-uri "http://github.com/ngrunwald/ring-middleware-format/blob/master/{filepath}#L{line}"
           :defaults {:doc/format :markdown}}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha14"]]}}
-  :aliases {"all" ["with-profile" "dev:dev,1.6:dev,1.7:dev,1.9"]})
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.10 {:dependencies [[org.clojure/clojure "1.10.0-RC4"]]}}
+  :aliases {"all" ["with-profile" "dev:dev,1.6:dev,1.7:dev,1.8:dev,1.10"]})

1c408bbbee55edcdfd714e0b29245a19f4e8ae94
Juho Teperi <juho.teperi@iki.fi>
2022-03-02 13:16:09 +0200

    Release 0.7.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-middleware-format "0.7.5-SNAPSHOT"
+(defproject ring-middleware-format "0.7.5"
   :description "Ring middleware for parsing parameters and emitting
   responses in various formats (mainly JSON, YAML and Transit out of
   the box)"
